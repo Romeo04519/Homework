7ac69edc31836ad76a19b33a7c54ecc4779d424f
Romeo04519 <firstlunatic@mail.ru>
2024-08-25 01:21:19 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -1,6 +1,6 @@
 def get_matrix(n = 1, m = 1, value = 1):
     matrix = []
-    if value <= 0: # задание в примечании возращать пустой список при аргументе <=0
+    if value <= 0 or n <= 0 or m  <= 0: # задание в примечании возращать пустой список при аргументе <=0
         return matrix
     else:
         for i in range(n):
@@ -12,6 +12,12 @@ def get_matrix(n = 1, m = 1, value = 1):
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
+result4 = get_matrix(0, 2, 13)
+result5 = get_matrix(4, 0, 13)
+result6 = get_matrix(4, 2, 0)
 print(result1)
 print(result2)
 print(result3)
+print(result4)
+print(result5)
+print(result6)
