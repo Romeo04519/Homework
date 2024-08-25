389709530d6f0c2cfb21d738ea7fbc97234bda21
Romeo04519 <firstlunatic@mail.ru>
2024-08-25 17:33:58 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as div_0
+from true_math import divide as div_inf
+
+result1 = div_0(69, 3)
+result2 = div_0(3, 0)
+result3 = div_inf(49, 7)
+result4 = div_inf(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
