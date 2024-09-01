dbd95082bd0824cff23d8a3fe2b58b67bd5aa366
Romeo04519 <firstlunatic@mail.ru>
2024-09-01 18:12:38 +0300

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,29 @@
+from Tools.scripts.md5sum import printsum
+
+
+def custom_write(file_name, string):
+    file = open(file_name, 'w', encoding = 'utf-8')
+    list_byte = []
+    string_positions = {}
+    number = 0
+    for i in string:
+        curs_byte = file.tell()
+        file.write(f'{i}\n')
+        string_positions[number+1, curs_byte] = i
+    return string_positions
+
+
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
+
+
