1b5efbf6afd9ab730b1145299ed7892274262859
Romeo04519 <firstlunatic@mail.ru>
2024-09-06 22:50:13 +0300

    Add files via upload

--- module_10_5.py
+++ module_10_5.py
@@ -4,23 +4,29 @@ import multiprocessing
 def read_info(name):
     all_data = []
     with open(name, 'r') as fl:
-        while fl.readline() != '':
-            s = fl.readline()
-            all_data.append(s)
-#
-# start = datetime.datetime.now()
+        data = fl.readline()
+        while data:
+            data = fl.readline()
+            all_data.append(data[0:-1])
+
+
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
-# for name in filenames:
-#     read_info(name)
-# end = datetime.datetime.now()
-# print(end-start)
 
-
-if __name__ == '__main_':
+if __name__ == '__main__':
     start = datetime.datetime.now()
-    with multiprocessing.Pool(processes=2) as pool:
-        filenames = [f'./file {number}.txt' for number in range(1, 5)]
+    for name in filenames:
+        read_info(name)
+    end = datetime.datetime.now()
+    print(end - start)
+
+    start = datetime.datetime.now()
+    with multiprocessing.Pool(processes=4) as pool:
         pool.map(read_info, filenames)
     end = datetime.datetime.now()
     print(end-start)
+
+
+
+
+
 
