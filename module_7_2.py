071ea7793977e251655733a68301e3b2e2c9dc62
Romeo04519 <firstlunatic@mail.ru>
2024-09-10 02:41:04 +0300

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -4,11 +4,12 @@ from Tools.scripts.md5sum import printsum
 def custom_write(file_name, string):
     file = open(file_name, 'w', encoding = 'utf-8')
     string_positions = {}
-    number = 0
+    number = 1
     for i in string:
         curs_byte = file.tell()
         file.write(f'{i}\n')
-        string_positions[number+1, curs_byte] = i
+        string_positions[number, curs_byte] = i
+        number += 1
     file.close()
     return string_positions
 
