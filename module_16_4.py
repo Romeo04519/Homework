77b0c3f3b8162419ddd7876af66301ca87b53437
Romeo04519 <firstlunatic@mail.ru>
2024-09-22 17:37:06 +0300

    Add files via upload

---
+++ module_16_4.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, Path, Body
+from typing import Annotated
+from typing import List
+from pydantic import BaseModel
+from starlette.exceptions import HTTPException
+
+app = FastAPI()
+
+users = []
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
+@app.get('/')
+async def first_welcome() -> str:
+    return 'Hello World'
+
+@app.get('/users')
+async def welcome() -> List[User]:
+    return users
+
+@app.post('/user/{username}/{age}')
+async def welcome_id_user(username: str, age = int) -> str:
+    User.id = len(users)+1
+    User.username = str(username)
+    User.age = age
+    users.append(User)
+    return f'User {User.id} is registered'
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def change_id_user(user_id: int, username: str, age: int) -> str:
+    try:
+        edit_user = users[user_id]
+        edit_user.username = username
+        edit_user.age = age
+        return f'User {user_id} has been updated'
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+@app.delete('/user/{user_id}')
+async def del_user(user_id: int) -> str:
+    try:
+        users.pop(user_id)
+        return f'User {user_id} has been deleted'
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+
+
+# python -m uvicorn module_16_3:app - запуск сервера
