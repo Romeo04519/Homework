5fadccd26da7c5d89d951d5ba7a54103f80043bf
Romeo04519 <firstlunatic@mail.ru>
2024-09-25 15:56:04 +0300

    Add files via upload

--- module_11_3.py
+++ module_11_3.py
@@ -7,20 +7,22 @@ class Inspec:
         self.obj = obj
 
     def introspection_info(self):
-        atr_list = []
+        #atr_list = []
         dict_obj = {}
-        type_ = type(self.obj)
+        type_ = type(self.obj).__name__
         dict_obj['type'] = type_
-        for i in dir(self.obj):
-            if inspect.isfunction(i) or inspect.ismethod(i):
-                continue
-            else:
-                atr_list.append(i)
-        dict_obj['attributes'] = atr_list
-        methods_ = inspect.getmembers(number_info, predicate=inspect.ismethod)
+        # for i in dir(self.obj):
+        #     if inspect.isfunction(i) or inspect.ismethod(i):
+        #         continue
+        #     else:
+        #         atr_list.append(i)
+        # dict_obj['attributes'] = atr_list
+        methods_ = inspect.getmembers(self.obj, predicate=inspect.isroutine)
+        atr = inspect.getmembers(self.obj, predicate=lambda x: not (inspect.isroutine(x)))
+        dict_obj['attributes'] = atr
         dict_obj['methods'] = methods_
         module_ = inspect.getmodule(self.obj)
-        dict_obj['module'] = module_
+        dict_obj['module'] = module_.__name__ if module_ else None
         return dict_obj
 
 number_info = Inspec(42)
