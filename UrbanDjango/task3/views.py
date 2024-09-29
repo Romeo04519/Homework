d4c72f1073fab1d54cbd2691e580ca56da4f49ab
Romeo04519 <firstlunatic@mail.ru>
2024-09-29 18:58:27 +0300

    Update views.py

--- UrbanDjango/task3/views.py
+++ UrbanDjango/task3/views.py
@@ -6,7 +6,15 @@ def main_page(request):
     return render(request, 'main_page.html')
 
 def books(request):
-    return render(request, 'books.html')
+    first = 'Граф Монте-Кристо'
+    second = 'Отверженные'
+    third = 'Гордость и предубеждения'
+    context={
+        'first': first,
+        'second': second,
+        'third': third,
+    }
+    return render(request, 'books.html', context)
 
 def about_us(request):
     return render(request, 'about_us.html')
